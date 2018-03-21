9ad4817d5323512a53daa3879817afc0585d0684
Hasan Pekdemir <hpekdemir.smart@googlemail.com>
2018-03-21 15:02:31 +0100

    update plugin skeleton

--- plugin_skeleton.go
+++ plugin_skeleton.go
@@ -3,16 +3,22 @@ package main
 import (
 	"log"
 	"net/rpc/jsonrpc"
+	"time"
 
 	"github.com/natefinch/pie"
+	"github.com/pebbe/zmq4"
+	"gopkg.in/mgo.v2/bson"
 )
 
-var stdio chan []string
+type Message struct {
+	Plugin    string            `json:"plugin"`
+	Timeout   time.Duration     `json:"timeout"`
+	Args      map[string]string `json:"args"`
+	Command   string            `json:"command"`
+	StdSocket string            `json:"stdsocket"`
+}
 
 func main() {
-	// initialize stdoi channel
-	stdio = make(chan []string)
-
 	// set prefix of module logger.
 	// the logs in the terminal where the worker is running will be prefixed with this string.
 	log.SetPrefix("[awesomeplugin log] ")
@@ -31,39 +37,43 @@ func main() {
 type api struct{}
 
 // NOTE: you HAVE to have a function called "Init". this is the entrance of the worker process calling your plugin
-func (api) Init(data []string, response *[]string) error {
+func (api) Init(data *Message, response *bson.M) error {
 	// check if we receive data at all or return.
 	// you can skip this if your plugin doesn't expect any input data
-	if len(data) == 0 {
+	if data == nil {
 		return nil
 	}
 
-	// if you get data, you will get your payload in data[0]
-	payload := data[0]
+	command := data.Command
+	_ = data.Args
 
-	// or, additionally, maybe the client can pass more than one argument. then you can work with data[0:]
-	// payload := data[0:]
+	// init zmq std socket for realtime std IO communication back to the client.
+	stdoutchan, err := zmq4.NewSocket(zmq4.PAIR)
+	if err != nil {
+		return err
+	}
+	err = stdoutchan.Connect(data.StdSocket)
+	if err != nil {
+		return err
+	}
+	defer stdoutchan.Close()
 
-	// do something with payload
-	log.Print(payload)
+	// now do something with the command given to your by the client.
+	log.Print(command)
 
 	// occasionally update the client with information in realtime
-	stdio <- []string{"look, what I'm doing..."}
+	stdoutchan.SendMessage("loog what I'm doing ma...")
 
-	// end realtime stream to client with "DONE" followed by a return code ("200" is hardcoded as success. the number is up to you.)
-	// NOTE: you HAVE to send a "DONE" followed by a return code in the same slice. also, you HAVE to send this message to end the stream to the client!!!
-	stdio <- []string{"DONE", "200"}
+	// Provide your exitcode and optionally your result if you have any results to give back.
+	*response = bson.M{
+		"exitcode": 200,
+		"result":   "",
+	}
 
-	// give something back to our worker process (not in use, yet.)
-	*response = []string{}
-	return nil
-}
+	// IMPORTANT:
+	// you MUST close the stdoutchan channel with a "CLOSE" message, otherwise bad things will happen!
+	stdoutchan.SendMessage("CLOSE")
 
-// NOTE: you HAVE to have a function called "Streams"
-// THIS FUNCTION HAS TO EXIST (DON'T change the function body)
-func (api) Streams(data []string, response *[]string) error {
-	line := <-stdio
-	*response = line
 	return nil
 }
 
